1076a2bc7c8f3b40c652276d6a0c6bc9c5e5f812
Jeroen van Vugt <jj.vanvugt@student.avans.nl>
2024-02-05 14:06:51 +0100

    Create drag & drop

---
+++ scripts/week-1/drag-and-drop.js
@@ -0,0 +1,57 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const draggables = document.getElementById('draggables');
+  draggables.addEventListener('dragstart', dragStartHandler);
+
+  const dropzones = document.getElementById('dropzones');
+  dropzones.addEventListener('click', clickHandler);
+  dropzones.addEventListener('dragstart', dragStartHandler);
+  dropzones.addEventListener('dragenter', dragEnterHandler);
+  dropzones.addEventListener('dragover', dragOverHandler);
+  dropzones.addEventListener('dragleave', dragLeaveHandler);
+  dropzones.addEventListener('drop', dropHandler);
+});
+
+function clickHandler(event) {
+  if (event.target.tagName !== 'IMG') { return; }
+
+  const element = event.target;
+  const pokemon = element.id.at(0).toUpperCase() + element.id.slice(1) + '!';
+  
+  alert(pokemon);
+}
+
+function dragStartHandler(event) {
+  event.dataTransfer.setData('text/plain', event.target.id);
+  event.dataTransfer.effectAllowed = 'move';
+}
+
+function dragEnterHandler(event) {
+  if (event.target.tagName !== 'TD') { return; }
+  event.target.className = 'has-background-primary-light';
+}
+
+function dragOverHandler(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = 'move';
+}
+
+function dragLeaveHandler(event) {
+  if (event.target.tagName !== 'TD') { return; }
+  event.target.removeAttribute('class');
+}
+
+function dropHandler(event) {
+  event.preventDefault();
+
+  if (event.target.children.length !== 0) { return; }
+
+  const data = event.dataTransfer.getData('text/plain');
+  const element = document.getElementById(data);
+
+  if (event.target.contains(element)) { return; }
+
+  event.target.appendChild(element);
+  event.target.removeAttribute('class');
+}
